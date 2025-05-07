4de910dce2b512dbb4c844807e3bf158e7452c5e
r-Lee6463 <rleebusiness1@gmail.com>
2025-05-07 18:58:48 -0500

    Delete Commerce-Bank-Project directory

--- Commerce-Bank-Project/src/main/java/org/BSB/com/service/EmailExistsException.java
+++
@@ -1,8 +0,0 @@
-package org.BSB.com.service;
-
-// Thrown when someone tries to register/update to an email that already exists
-public class EmailExistsException extends RuntimeException {
-    public EmailExistsException(String message) {
-        super(message);
-    }
-}
